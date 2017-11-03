35108ef8037fb9d492052dbe224b07ef1f0126e3
Nick Paolino <github email address>
2017-11-02 21:10:16 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  let keystrokes = [];
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', function(ev){
+    keystrokes.push(ev.which);
+    if (JSON.stringify(code) === JSON.stringify(keystrokes.slice(-10))){
+      alert("Konami Found!")
+    }
+  })
 }
